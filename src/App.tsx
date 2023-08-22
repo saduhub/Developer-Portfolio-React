93c3d3c555ac922576d90d2d11b22809f2da927d
Jorge Duarte <sadujorge@gmail.com>
2023-08-22 16:19:40 -0700

    Set up Shadcn envirenment. Using Vite, TS, React, Radix, Tailwind.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ButtonDemo } from './components/Button'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <ButtonDemo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+ 
+export function ButtonDemo() {
+  return <Button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">Button</Button>
+}
