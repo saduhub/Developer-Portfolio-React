fcc359044b35de1b6e3253b12a3cea8969da5b67
Jorge Duarte <sadujorge@gmail.com>
2023-08-22 23:55:26 -0700

    Added initial navigation logic using React hooks as well as function from react-router-dom. Ensures About me is initial page on load.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import { ButtonDemo } from './components/Button'
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 // Import to be able to use routing.
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar"
 import Landing from "./components/Landing"
@@ -16,6 +16,8 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        {/* Perform initial navigation */}
+        <Navigation/>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Landing />} />
@@ -28,6 +30,22 @@ function App() {
       </BrowserRouter>
     </>
   )
+}
+
+function Navigation() {
+  // Use method from react-router-dom to navigate to a specific url.
+  const navigateTo = useNavigate();
+  // useState to store initial value of navigatedToAbout and updater function to update navigatedToAbout when it is true. 
+  const [navigatedToAbout, navigationComplete] = useState(false);
+  // Hook used to manipulate value of navigatedToAbout by checking the value of navigatedToAbout as specified in the array.
+  useEffect(() => {
+    if (!navigatedToAbout) {
+      navigateTo('/about');
+      navigationComplete(true);
+    }
+  }, [navigatedToAbout, navigateTo]);
+  // Nothing rendered
+  return null;
 }
 
 export default App
