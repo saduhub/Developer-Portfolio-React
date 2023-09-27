7aa7bb116da6dfa4c9ad1e6b45defb10077cf342
Jorge Duarte <sadujorge@gmail.com>
2023-09-26 19:27:08 -0700

    Tried refactoring css but images would not show inless I added them as backgrounf to div. Will refactor later

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,45 +1,22 @@
+import React from 'react';
+import './About.css';
 import profile from '/images/profile.png';
 
-const styles = {
-    sectionStyle: {
-      backgroundColor: 'black',
-    },
-    divStyle: {
-      textShadow: '0px 0px 4px var(--purple-font-color)',
-      maxWidth: '50rem',
-      margin: '5rem auto'
-    },
-    h3Style: {
-      textAlign: 'center',
-    },
-    pStyle: {
-      textAlign: 'center',
-      margin: '1rem 5rem',
-    },
-    imageDivStyle: {
-        display: 'flex',
-        padding: '1rem',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    imageStyle: {
-        maxWidth: '20rem',
-        height: 'auto',
-        padding: '5px',
-        margin: '0px 20px',
-        borderRadius: '1rem',
-    },
-};
-  
 function About() {
     return (
-        <section style={styles.sectionStyle} >
-            <div style={styles.divStyle}>
-                <h3 style={styles.h3Style}>About me</h3>
-                <p style={styles.pStyle}>Hello! I'm a budding full-stack developer taking my first steps into the exciting world of web development. While I may be at the start of my journey, I bring a strong work ethic, creative flair, and ambition to the table.As someone who's transitioning their programming hobby into a full-time career, I can say with enthusiasm that I'm truly excited about the possibilities. My dedication to programming has evolved from a pastime into a passion for programming, new technologies, and the world of development. When I'm not coding, you'll find me chasing platinum trophies or cheering on my favorite sports teams. I describe myself as a student first and with each line of code, I'm excited to create an application better than the one before. My eagerness to learn and adapt helps me stay on top of industry trends and write code that I hope will improve user experience. I'm excited about the path ahead, and I'm confident in my ability to grow and contribute to the community. Let's connect and explore the exciting possibilities that lie ahead!</p>
+        <section className="aboutSectionStyle">
+            <div className="aboutDivStyle">
+                <h3 className="aboutH3Style">About me</h3>
+                <p className="aboutPStyle">
+                  Hello! I'm a budding full-stack developer taking my first steps into the exciting world of web development. While I may be at the start of my journey, I bring a strong work ethic, creative flair, and ambition to the table.As someone who's transitioning their programming hobby into a full-time career, I can say with enthusiasm that I'm truly excited about the possibilities. My dedication to programming has evolved from a pastime into a passion for programming, new technologies, and the world of development. When I'm not coding, you'll find me chasing platinum trophies or cheering on my favorite sports teams. I describe myself as a student first and with each line of code, I'm excited to create an application better than the one before. My eagerness to learn and adapt helps me stay on top of industry trends and write code that I hope will improve user experience. I'm excited about the path ahead, and I'm confident in my ability to grow and contribute to the community. Let's connect and explore the exciting possibilities that lie ahead!
+                </p>
             </div>
-            <div style={styles.imageDivStyle}>
-                <img src={profile} alt="Jorge Duarte" style={styles.imageStyle}/>
+            <div className="aboutImageDivStyle">
+                <div 
+                    className="aboutImageStyle"
+                    style={{ backgroundImage: `url(${profile})` }}
+                    alt="Jorge Duarte">
+                </div>
             </div>
         </section> 
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,12 @@
+import React from 'react';
+import './Projects.css';
+
 import songSensei from '/images/songsensei.png';
 import tuneStack from '/images/tunestack.png';
 import careconnect from '/images/careconnect.png';
 import scheduler from '/images/scheduler.png';
 import editor from '/images/editor.png';
 import weather from '/images/weather.png';
-
-const styles = {
-  sectionStyle: {
-    backgroundColor: 'black',
-  },
-  AllProjectsDivStyle: {
-    display: 'flex', 
-    flexDirection: 'column', 
-    alignItems: 'center', 
-    justifyContent: 'center', 
-    height: '100%',
-  },
-  projectDivStyle: {
-    margin: '2rem',
-    padding: '1rem',
-    backgroundColor: 'rgba(30, 30, 30, 0.8)',
-    borderRadius: '1rem',
-    maxWidth: '20rem'
-  },
-  imageStyle: {
-    maxWidth: '15rem',
-    height: 'auto',
-    padding: '5px',
-    margin: '0px 20px',
-    borderRadius: '1rem',
-  },
-  divStyle: {
-    textShadow: '0px 0px 4px var(--purple-font-color)',
-    margin: '5rem',
-  },
-  h2Style: {
-    textAlign: 'center',
-  },
-  pStyle: {
-    textAlign: 'center',
-    margin: '1rem auto',
-    maxWidth: '20rem'
-  },
-};
 
 const projects = {
   songSensei: {
@@ -85,27 +49,34 @@ const projects = {
 
 function Projects() {
   return (
-    <section style={styles.sectionStyle}>
-      <div style={styles.AllProjectsDivStyle}>
-      {/* Iterate over each project. Create specified html for each project and assign identifying key based on the keys found in projects object. */}
-      {Object.keys(projects).map((projectKey) => {
-        const project = projects[projectKey];
-        return (
-          <div key={projectKey} style={styles.projectDivStyle}>
-            <h2 style={styles.h2Style}>{project.title}</h2>
-            <p style={styles.pStyle}>{project.description}</p>
-            <div>
-              <a href={project.link}>
-                <img src={project.image} alt={`${project.title} screenshot`} style={styles.imageStyle}/>
-              </a>
+    <section className="projectsSectionStyle">
+      <div className="projectsAllProjectsDivStyle">
+        {Object.keys(projects).map((projectKey) => {
+          const project = projects[projectKey];
+          return (
+            <div key={projectKey} className="projectsProjectDivStyle">
+              <h2 className="projectsH2Style">{project.title}</h2>
+              <p className="projectsPStyle">{project.description}</p>
+              <div>
+                <a href={project.link}>
+                  <div 
+                    className="projectsImageStyle"
+                    style={{ backgroundImage: `url(${project.image})` }}
+                    alt={`${project.title} screenshot`}>
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       </div>
-      <div style={styles.divStyle}>
-        <h2 style={styles.h2Style}>Technologies Used</h2>
-        <p style={styles.pStyle}>JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, JQuery, Bootstrap, Tailwind CSS, DOM, APIs, JSON, AJAX, OOP, MVC, Sequelize, Jest test suites, git, npm, Mongoose.</p>
+      <div className="projectsDivStyle">
+        <h2 className="projectsH2Style">Technologies Used</h2>
+        <p className="projectsPStyle">
+          JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, React, 
+          Node, JQuery, Bootstrap, Tailwind CSS, DOM, APIs, JSON, AJAX, OOP, MVC, Sequelize, 
+          Jest test suites, git, npm, Mongoose.
+        </p>
       </div>
     </section>
   );
